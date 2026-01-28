a284fb915bff2922e738eb89762da4ecba6f2498
kkchemboli <krishnakoundinyachemboli@Krishnas-MacBook-Pro.local>
2026-01-28 18:53:07 +0530

    updated navbar

--- frontend/src/component/Navbar/navbar.jsx
+++ frontend/src/component/Navbar/navbar.jsx
@@ -8,33 +8,23 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-  <nav className="nav-flex">
-    <div className="nav-left">
-    <h1>EdTube</h1>
-      {/* <img src={menu_icon} alt="menu icon" className="menu-icon"/> */}
-    </div>
-
-  <div className="nav-center">
-    {/* <img src={home_icon} alt="home icon" class="home-icon"/> */}
-  </div>
-  
-    <div className="nav-right">
-      <Link to="/" className="nav-pill">
-        <img src={home_icon} alt="home icon" />
-        <span>Home</span>
-      </Link>
-      
-      <div className="nav-pill">
-        <img src={more_icon} alt="more icon" />
-        <span>More</span>
+    <nav className="nav-flex">
+      <div className="nav-left">
+        <h1>EdTube</h1>
+        {/* <img src={menu_icon} alt="menu icon" className="menu-icon"/> */}
       </div>
 
-      <div className="nav-pill">
-        <img src={profile_icon} alt="profile icon" />
-        <span>Sign In</span>
+      <div className="nav-center">
+        {/* <img src={home_icon} alt="home icon" class="home-icon"/> */}
       </div>
-    </div>
-  </nav>
+
+      <div className="nav-right">
+        <Link to="/" className="nav-pill">
+          <img src={home_icon} alt="home icon" />
+          <span>Home</span>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
